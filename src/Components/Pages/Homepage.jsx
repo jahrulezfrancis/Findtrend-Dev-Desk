2694f6c5ee571d0d61d815184a0af9f4c5d36e9b
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-21 12:55:40 +0100

    Initial Commit

---
+++ src/Components/Pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+import IntroSection from '../Header/Introsection';
+
+export default function Homepage() {
+    return (
+        <Box>
+            <IntroSection />
+        </Box>
+    )
+}
