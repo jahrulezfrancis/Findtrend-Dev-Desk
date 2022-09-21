7f0b5f4da7865373889f83e11d6e040c51456dfb
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-21 19:13:32 +0100

    Added the greeting bar

--- src/Components/Header/Introsection.jsx
+++ src/Components/Header/Introsection.jsx
@@ -1,11 +1,13 @@
 import { Box } from '@chakra-ui/react';
 import React from 'react';
 import Navigation from './Navigation';
+import { WelcomeBox } from './Navigation';
 
 export default function IntroSection() {
     return(
-        <Box>
+        <Box bgColor='#000000' minH='56em'>
             <Navigation />
+            <WelcomeBox />
         </Box>
     )
 }
