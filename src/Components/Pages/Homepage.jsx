1632b2a400772cfc7e2d5b0f016b57b55c4f9f53
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-21 21:45:29 +0100

    Added tab section

--- src/Components/Pages/Homepage.jsx
+++ src/Components/Pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import { Box } from '@chakra-ui/react';
 import React from 'react';
 import IntroSection from '../Header/Introsection';
+import { TabSection } from '../Sections/FrontPage';
 
 export default function Homepage() {
     return (
         <Box>
             <IntroSection />
+            <TabSection />
         </Box>
     )
 }
