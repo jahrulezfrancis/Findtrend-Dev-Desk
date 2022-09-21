26aa4514a607d16da1f6d71d6f1670afa8201e06
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-21 19:14:11 +0100

    Created the welcome Box component

--- src/Components/Header/Navigation.jsx
+++ src/Components/Header/Navigation.jsx
@@ -1,42 +1,72 @@
-import { HStack, Box, ListItem, UnorderedList, Image, Button } from '@chakra-ui/react'
+import { HStack, Box, ListItem, UnorderedList, Image, Button, Spacer, VStack, Heading, Text } from '@chakra-ui/react'
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import HeaderLogo from "../Images/header-logo.png"
+import TwitterDesign from "../Images/log-twitter.png"
+import FacebookDesign from "../Images/log-facebook.png"
+import PinterestDesign from "../Images/log-pinterest.png"
+import BaseballDesign from "../Images/log-elo-musk.png"
 
 
 export default function Navigation() {
     return (
         <Box>
             <UnorderedList listStyleType='none' mr='.8em' p='1em'>
-                <HStack>
+                <HStack justify=''>
                     <ListItem>
                         <NavLink>
                             <Image src={HeaderLogo} alt='' />
                         </NavLink>
                     </ListItem>
-                    <ListItem>
-                        <NavLink>About</NavLink>
-                    </ListItem>
-                    <ListItem>
-                        <NavLink>How it works</NavLink>
-                    </ListItem>
-                    <ListItem>
-                        <NavLink>Pricing </NavLink>
-                    </ListItem>
-                    <ListItem>
-                        <NavLink>Solution</NavLink>
-                    </ListItem>
-                    <ListItem>
-                        <NavLink>Features</NavLink>
-                    </ListItem>
+                    <Spacer />
+                    <HStack spacing='2em'>
+                        <ListItem color='white' fontFamily='Effre' fontWeight='400' fontSize='1em'>
+                            <NavLink >About</NavLink>
+                        </ListItem>
+                        <ListItem color='white' fontFamily='Effre' fontWeight='400' fontSize='1em'>
+                            <NavLink>How it works</NavLink>
+                        </ListItem>
+                        <ListItem color='white' fontFamily='Effre' fontWeight='400' fontSize='1em'>
+                            <NavLink color='white' fontFamily='Effre' fontWeight='400' fontSize='1em'>Pricing </NavLink>
+                        </ListItem>
+                        <ListItem color='white' fontFamily='Effre' fontWeight='400' fontSize='1em'>
+                            <NavLink>Solution</NavLink>
+                        </ListItem>
+                        <ListItem color='white' fontFamily='Effre' fontWeight='400' fontSize='1em'>
+                            <NavLink>Features</NavLink>
+                        </ListItem>
+                    </HStack>
+                    <Spacer />
                     <Box>
                         <HStack>
-                            <Button>Login</Button>
-                            <Button>Register</Button>
+                            <Button bgColor='black' color='white' _hover='none' borderRadius='2em'>Login</Button>
+                            <Button bgColor='white' color='black' borderRadius='2em' w='8em'>Register</Button>
                         </HStack>
                     </Box>
                 </HStack>
             </UnorderedList>
+        </Box>
+    )
+}
+
+export function WelcomeBox() {
+    return (
+        <Box>
+            <VStack>
+                <Heading color='white'>Minimize your tabs.</Heading>
+                <Heading color='white'>Find the trends!</Heading>
+                <Text color='white'>
+                    Don’t let your computer memories consumes all of those browser tabs. <br />
+                    Findtrend  let you gathers all of your favorite website into one place.
+                </Text>
+                <Button p='1em' bgColor='#A8FF35' borderRadius='2em' w='10em' h='3em'>Get started</Button>
+                <HStack w='auto' m='3em'>
+                    <Image w='18em' src={TwitterDesign} alt='' />
+                    <Image pt='5em'  w='18em' src={PinterestDesign} alt='' />
+                    <Image w='18em' src={FacebookDesign} alt='' />
+                    <Image pt='5em' w='18em' src={BaseballDesign} alt='' />
+                </HStack>
+            </VStack>
         </Box>
     )
 }
