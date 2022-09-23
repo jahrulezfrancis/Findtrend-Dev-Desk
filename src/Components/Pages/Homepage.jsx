399916b4190a717c40fe06576b9d4be66d13b793
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-23 23:46:30 +0100

    updated the user interface

--- src/Components/Pages/Homepage.jsx
+++ src/Components/Pages/Homepage.jsx
@@ -1,13 +1,14 @@
 import { Box } from '@chakra-ui/react';
 import React from 'react';
 import IntroSection from '../Header/Introsection';
-import { TabSection } from '../Sections/FrontPage';
+import { AfterTabSection, TabSection } from '../Sections/FrontPage';
 
 export default function Homepage() {
     return (
         <Box>
             <IntroSection />
             <TabSection />
+            <AfterTabSection />
         </Box>
     )
 }
