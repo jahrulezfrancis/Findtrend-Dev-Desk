460120d76a6fe28aee2a77ee863621b28122e09e
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-26 23:42:57 +0100

    added the newsletter component

--- src/Components/Pages/Homepage.jsx
+++ src/Components/Pages/Homepage.jsx
@@ -1,6 +1,7 @@
 import { Box } from '@chakra-ui/react';
 import React from 'react';
 import IntroSection from '../Header/Introsection';
+import { PricingList } from '../Sections/Body';
 import { AfterTabSection, TabSection } from '../Sections/FrontPage';
 
 export default function Homepage() {
@@ -9,6 +10,7 @@ export default function Homepage() {
             <IntroSection />
             <TabSection />
             <AfterTabSection />
+            <PricingList />
         </Box>
     )
 }
