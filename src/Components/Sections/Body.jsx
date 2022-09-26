e3ee699c43b1754c1b4cc2cf04e47dee07226a02
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-26 23:59:23 +0100

    updated pricing section

--- src/Components/Sections/Body.jsx
+++ src/Components/Sections/Body.jsx
@@ -1,7 +1,19 @@
-import { Box } from "@chakra-ui/react";
+import { useState } from "react";
+import { Box, Heading, VStack, Stack, Switch, Text } from "@chakra-ui/react";
+
 
 export function PricingList() {
-    return(
-        <Box>Yeah the Pricing list you know</Box>
+    const [selected, setselected] = useState(false);
+    return (
+        <Box bgColor='#000000'>
+            <VStack>
+                <Heading color='white'>Get Your Best Deals</Heading>
+                <Stack align='center' direction='row'>
+                    <Text color={selected ? 'white' : 'green'} _active={{color: 'white'}}>Monthly</Text>
+                    <Switch size='lg' onChange={() => { selected === false ? setselected('true') : setselected(false)}} />
+                    <Text color='white'>Yearly</Text>
+                </Stack>
+            </VStack>
+        </Box>
     )
 }
