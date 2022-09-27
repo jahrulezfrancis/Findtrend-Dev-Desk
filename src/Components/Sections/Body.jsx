e3a5cb35e073068cbe44a540b18d6ed3f4186ede
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-27 01:32:36 +0100

    Finished pricing template

--- src/Components/Sections/Body.jsx
+++ src/Components/Sections/Body.jsx
@@ -1,34 +1,39 @@
 import { useState } from "react";
-import { Box, Heading, VStack, Stack, Switch, Text, Divider, HStack, List, ListItem, ListIcon, Button } from "@chakra-ui/react";
+import { Box, Heading, VStack, Stack, Switch, Text, Divider, HStack, List, ListItem, ListIcon, Button, Spacer } from "@chakra-ui/react";
 import { BsCheckCircle } from "react-icons/bs";
 
 
 function PricingTemp() {
     return (
-        <Box bgColor='#FAFAFA' minH='40em' w='21em' margin='2em' borderRadius='1em'>
-            <VStack>
-                <Heading>Personal</Heading>
-                <Text>Special first packet for all</Text>
-                <Divider />
-                <HStack>
-                    <Heading>$8/</Heading>
-                    <Text pt='1em' fontSize='1em'>Month</Text>
-                </HStack>
-                <List spacing={3}>
-                    <ListItem>
-                        <ListIcon as={BsCheckCircle} color='black.500' outline='fill' />
-                        Up to 5 page each group
-                    </ListItem>
-                    <ListItem>
-                        <ListIcon as={BsCheckCircle} color='black.500' outline='fill' />
-                        Up to 10 group page
-                    </ListItem>
-                    <ListItem>
-                        <ListIcon as={BsCheckCircle} color='black.500' outline='fill' />
-                        5 Days group page saved
-                    </ListItem>
-                </List>
-                <Button>Start free trial</Button>
+        <Box bgColor='#FAFAFA' h='30em' minH='30em' w='21em' margin='2em' borderRadius='1em'>
+            <VStack h='100%'>
+                <Stack align='center' justify='center'>
+                    <Heading pt='1em'>Personal</Heading>
+                    <Text>Special first packet for all</Text>
+                    <Divider />
+                    <HStack justify='center' align='center' pt='3em'>
+                        <Heading>$8/</Heading>
+                        <Text pt='1em' fontSize='1em'>Month</Text>
+                    </HStack>
+                    <List spacing={3}>
+                        <ListItem>
+                            <ListIcon as={BsCheckCircle} color='black.500' outline='fill' />
+                            Up to 5 page each group
+                        </ListItem>
+                        <ListItem>
+                            <ListIcon as={BsCheckCircle} color='black.500' outline='fill' />
+                            Up to 10 group page
+                        </ListItem>
+                        <ListItem>
+                            <ListIcon as={BsCheckCircle} color='black.500' outline='fill' />
+                            5 Days group page saved
+                        </ListItem>
+                    </List>
+                </Stack>
+                <Spacer />
+                <Box align='end' justify='end' p='2em'>
+                    <Button>Start free trial</Button>
+                </Box>
             </VStack>
         </Box>
     )
@@ -37,7 +42,7 @@ function PricingTemp() {
 export function PricingList() {
     const [selected, setselected] = useState(true);
     return (
-        <Box bgColor='#000000'>
+        <Box bgColor='#000000' p='2em'>
             <VStack>
                 <Heading color='white'>Get Your Best Deals</Heading>
                 <Stack align='center' direction='row'>
