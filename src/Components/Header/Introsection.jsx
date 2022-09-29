015050b60bc9d6019f8e1af59a4953c18dec9336
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-29 16:57:03 +0100

    Added mobile navigation

--- src/Components/Header/Introsection.jsx
+++ src/Components/Header/Introsection.jsx
@@ -1,12 +1,13 @@
 import { Box } from '@chakra-ui/react';
 import React from 'react';
-import Navigation from './Navigation';
+import Navigation, { MobileMenu } from './Navigation';
 import { WelcomeBox } from './Navigation';
 
 export default function IntroSection() {
     return(
         <Box bgColor='#000000' minH='50em'>
             <Navigation />
+            <MobileMenu />
             <WelcomeBox />
         </Box>
     )
